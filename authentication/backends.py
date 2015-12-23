037bca9e17b5eff7e907d0eb7c35e74010290f3c
Adarnof <adarnof@gmail.com>
2015-12-23 22:47:00 +0000

    Removed unnecessary typecasting for logging messages.

--- authentication/backends.py
+++ authentication/backends.py
@@ -48,15 +48,15 @@ class AuthenticationBackend(object):
         #check if character model exists to return user
         if EVECharacter.objects.filter(id=character_id).exists():
             character = EVECharacter.objects.get(id=character_id)
-            logger.debug("EVECharacter model exists for character id %s" % str(character_id))
+            logger.debug("EVECharacter model exists for character id %s" % character_id)
             if character.user:
-                logger.debug("Retrieved user for character id %s" % str(character_id))
+                logger.debug("Retrieved user for character id %s" % character_id)
                 return character.user
         #user does not exist for that character
-        logger.debug("No user found for character id %s" % str(character_id))
+        logger.debug("No user found for character id %s" % character_id)
         user = get_user_model().objects.create_user(main_character_id=character_id)
         user.save()
-        logger.debug("Retrieved user for character id %s" % str(character_id))
+        logger.debug("Retrieved user for character id %s" % character_id)
         return user
 
     #internet says I need this
