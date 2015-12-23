0708a1e4b155db464d22239063282522f3c5603b
Adarnof <adarnof@gmail.com>
2015-12-23 21:15:16 +0000

    Moved EVECharacter and EVECorporaton update functions to model. Changed ID fields to PositiveIntegerFields to play nicely with evelink. Untracked migrations folder. Updated logging to remove unnecessary typecasting.

--- eveonline/managers.py
+++ eveonline/managers.py
@@ -1,6 +1,4 @@
-from models import EVECharacter
-from models import EVECorporation
-from models import EVEAlliance
+from models import EVECharacter, EVECorporation, EVEAlliance, EVEApiKeyPair
 import evelink.api
 import evelink.char
 import evelink.eve
@@ -13,76 +11,47 @@ class EVEManager:
     @staticmethod
     def get_character_by_id(character_id):
         if EVECharacter.objects.filter(id=character_id).exists():
-            logger.debug("Returning existing character model with id %s" % str(character_id))
+            logger.debug("Returning existing character model with id %s" % character_id)
             return EVECharacter.objects.get(id=character_id)
         else:
-            logger.debug("No character model exists for id %s - triggering creation." % str(character_id))
+            logger.debug("No character model exists for id %s - triggering creation." % character_id)
             EVEManager.create_characters([character_id])
-            logger.debug("Returning new character model with id %s" % str(character_id))
+            logger.debug("Returning new character model with id %s" % character_id)
             return EVECharacter.objects.get(id=character_id)
 
     @staticmethod
     def create_characters(character_ids):
-        api = evelink.eve.EVE()
-        result = api.affiliations_for_characters(character_ids).result
-        logger.debug("Retrieved affiliations for character ids: %s" % str(character_ids))
         for id in character_ids:
             char, created = EVECharacter.objects.get_or_create(id = id)
             if created:
-                logger.info("Created model for character id %s" % str(id))
+                logger.info("Created model for character id %s" % id)
             else:
-                logger.warn("Attempting to create existing model for character id %s" % str(id))
-        EVEManager.update_characters(result)
+                logger.warn("Attempting to create existing model for character id %s" % id)
+            char.update()
 
     @staticmethod
-    def update_characters(result):
-        for id in result:
-            EVEManager.update_character(id)
-
-    @staticmethod
-    def update_character(character_id):
-        logger.debug("Initiating character update for id %s" % str(character_id))
-        api = evelink.eve.EVE()
-        result = api.affiliations_for_characters(character_id).result
-        logger.debug("Retrieved affiliations for character id %s" % str(character_id))
-        char = EVEManager.get_character_by_id(character_id)
-        logger.debug("Retrieved character model with id %s" % str(character_id))
-        char.name = result[character_id]['name']
-        char.corp_id = result[character_id]['corp']['id']
-        char.corp_name = result[character_id]['corp']['name']
-        if 'faction' in result[character_id]:
-            char.faction_id = result[character_id]['faction']['id']
-            char.faction_name = result[character_id]['faction']['name']
-        else:
-            logger.debug("No faction data found for character id %s. Blanking" % str(character_id))
-            char.faction_id = None
-            char.faction_name = None
-        if 'alliance' in result[character_id]:
-            char.alliance_id = result[character_id]['alliance']['id']
-            char.alliance_name = result[character_id]['alliance']['name']
-        else:
-            logger.debug("No alliance data found for character id %s. Blanking." % str(character_id))
-            char.alliance_id = None
-            char.alliance_name = None
-        logger.info("Finished updating character id %s from api." % str(character_id))
-        char.save()
+    def update_characters(character_ids):
+        for id in character_ids:
+            EVEManager.get_character_by_id(id).update()
 
     @staticmethod
     def assign_character_user(character_id, user):
         character = EVEManager.get_character_by_id(character_id)
+        if character.user:
+            logger.warn("Reassigning character from user %s to %s" % (char.user, user))
         character.user = user
         character.save()
-        logger.info("Assigned character id %s to user %s" % (str(character_id), str(user)))
+        logger.info("Assigned character id %s to user %s" % (character_id, user))
 
     @staticmethod
     def get_corp_by_id(corp_id):
         if EVECorporation.objects.filter(id=corp_id).exists():
-            logger.debug("Returning existing corp model with id %s" % str(corp_id))
+            logger.debug("Returning existing corp model with id %s" % corp_id)
             return EVECorporation.objects.get(id=corp_id)
         else:
-            logger.debug("No corp model exists for id %s - triggering creation." % str(corp_id))
+            logger.debug("No corp model exists for id %s - triggering creation." % corp_id)
             EVEManager.create_corps([corp_id])
-            logger.debug("Returning new corp model with id %s" % str(corp_id))
+            logger.debug("Returning new corp model with id %s" % corp_id)
             return EVECorporation.objects.get(id=corp_id)
 
     @staticmethod
@@ -90,37 +59,25 @@ class EVEManager:
         for id in corp_ids:
             corp, created = EVECorporation.objects.get_or_create(id = id)
             if created:
-                logger.info("Created model for corp id %s" % str(id))
+                logger.info("Created model for corp id %s" % id)
             else:
-                logger.warn("Attempting to create existing model for corp id %s" % str(id))
-            EVEManager.update_corp(id)
+                logger.warn("Attempting to create existing model for corp id %s" % id)
+            corp.update()
 
     @staticmethod
-    def update_corp(corp_id):
-        logger.debug("Updating corp info for corp id %s" % str(corp_id))
-        a = evelink.api.API()
-        api = evelink.corp.Corp(a)
-        result = api.corporation_sheet(corp_id=corp_id).result
-        logger.debug("Got corporation sheet from api for corp id %s: name %s ticker %s members %s" % (result['id'], result['name'], result['ticker'], result['members']['current']))
-        corp = EVEManager.get_corp_by_id(corp_id)
-        logger.debug("Got corp models for id %s" % str(corp_id))
-        corp.name = result['name']
-        corp.alliance_id = result['alliance']['id']
-        corp.alliance_name = result['alliance']['name']
-        corp.members = result['members']['current']
-        corp.ticker = result['ticker']
-        corp.save()
-        logger.info("Updated corp info for %s" % str(corp))
+    def update_corps(corp_ids):
+        for id in corp_ids:
+            EVEManager.get_corp_by_id(id).update()
 
     @staticmethod
     def get_alliance_by_id(alliance_id):
         if EVEAlliance.objects.filter(id=alliance_id).exists():
-            logger.debug("Returning existing alliance model with id %s" % str(alliance_id))
+            logger.debug("Returning existing alliance model with id %s" % alliance_id)
             return EVEAlliance.objects.get(alliance_id=alliance_id)
         else:
-            logger.debug("No alliance model exists for id %s - triggering creation." % str(alliance_id))
+            logger.debug("No alliance model exists for id %s - triggering creation." % alliance_id)
             EVEManager.create_alliances([alliance_id])
-            logger.debug("Returning new alliance model with id %s" % str(alliance_id))
+            logger.debug("Returning new alliance model with id %s" % alliance_id)
             return EVEAlliance.objects.get(id=alliance_id)
 
     @staticmethod
@@ -128,9 +85,9 @@ class EVEManager:
         for id in alliance_ids:
             alliance, created = EVEAlliance.objects.get_or_create(id = id)
             if created:
-                logger.info("Created model for alliance id %s" % str(id))
+                logger.info("Created model for alliance id %s" % id)
             else:
-                logger.warn("Attempting to create existing model for alliance id %s" % str(id))
+                logger.warn("Attempting to create existing model for alliance id %s" % id)
             EVEManager.update_alliances([id])
 
     @staticmethod
@@ -144,8 +101,8 @@ class EVEManager:
                 alliance.name = result[id]['name']
                 alliance.ticker = result[id]['ticker']
                 alliance.save()
-                logger.info("Updated alliance info for %s" % str(alliance))
+                logger.info("Updated alliance info for %s" % alliance)
             else:
                 logger.info("Alliance %s no longer exists. Deleting model." % id)
                 alliance.delete()
-        logger.debug("Finished updating alliance models %s" % str(ids))
+        logger.debug("Finished updating alliance models %s" % ids)
--- eveonline/models.py
+++ eveonline/models.py
@@ -2,53 +2,95 @@ from __future__ import unicode_literals
 
 from django.db import models
 import logging
+import evelink
 
 logger = logging.getLogger(__name__)
 
 class EVECharacter(models.Model):
-    id = models.CharField(max_length=254, primary_key=True)
+    id = models.PositiveIntegerField(primary_key=True)
     name = models.CharField(max_length=254)
-    corp_id = models.CharField(max_length=254)
-    corp_name = models.CharField(max_length=254)
-    alliance_id = models.CharField(max_length=254, null=True)
+    corp_id = models.PositiveIntegerField(null=True)
+    corp_name = models.CharField(max_length=254, null=True)
+    alliance_id = models.PositiveIntegerField(null=True)
     alliance_name = models.CharField(max_length=254, null=True)
-    faction_id = models.CharField(max_length=254, null=True)
+    faction_id = models.PositiveIntegerField(null=True)
     faction_name = models.CharField(max_length=254, null=True)
     user = models.ForeignKey('authentication.User', null=True)
     def __unicode__(self):
-        if self.character_name:
+        if self.name:
             return self.name.encode('utf-8')
         else:
             logger.warn("Character name missing in character model for id %s - returning id as __unicode__" % self.id)
             return self.id.encode('utf-8')
+    def update(self):
+        logger.debug("Initiating update for character model with id %s" % str(self.id))
+        api = evelink.eve.EVE()
+        result = api.affiliations_for_characters(self.id).result
+        if not self.id in result:
+            logger.error("API result does not contain this character id %s. Aborting update. Result: %s" % (self.id, str(result)))
+        else:
+            char_info = result[self.id]
+            logger.debug("Retrieved affiliations for character id %s: %s" % (self.id, char_info))
+            self.name = char_info['name']
+            self.corp_id = char_info['corp']['id']
+            self.corp_name = char_info['corp']['name']
+            if 'faction' in char_info:
+                self.faction_id = char_info['faction']['id']
+                self.faction_name = char_info['faction']['name']
+            else:
+                logger.debug("No faction data found for character id %s. Blanking" % self.id)
+                self.faction_id = None
+                self.faction_name = None
+            if 'alliance' in char_info:
+                self.alliance_id = char_info['alliance']['id']
+                self.alliance_name = char_info['alliance']['name']
+            else:
+                logger.debug("No alliance data found for character id %s. Blanking." % self.id)
+                self.alliance_id = None
+                self.alliance_name = None
+            logger.info("Finished updating character id %s from api." % self.id)
+            self.save()
 
 class EVECorporation(models.Model):
-    id = models.CharField(max_length=254, primary_key=True)
-    name = models.CharField(max_length=254)
-    alliance_id = models.CharField(max_length=254, null=True)
+    id = models.PositiveIntegerField(primary_key=True)
+    name = models.CharField(max_length=254, null=True)
+    alliance_id = models.PositiveIntegerField(null=True)
     alliance_name = models.CharField(max_length=254, null=True)
-    members = models.CharField(max_length=254)
-    ticker = models.CharField(max_length=254)
+    members = models.PositiveIntegerField(null=True)
+    ticker = models.CharField(max_length=254, null=True)
     def __unicode__(self):
         if self.name:
             return self.name.encode('utf-8')
         else:
             logger.warn("Corp name missing in corp model for id %s - returning id as __unicode__" % self.id)
             return self.id.encode('utf-8')
+    def update(self):
+        logger.debug("Updating corp info for corp id %s" % self.id)
+        a = evelink.api.API()
+        api = evelink.corp.Corp(a)
+        result = api.corporation_sheet(corp_id=self.id).result
+        logger.debug("Got corporation sheet from api for corp id %s: name %s ticker %s members %s" % (result['id'], result['name'], result['ticker'], result['members']['current']))
+        self.name = result['name']
+        self.alliance_id = result['alliance']['id']
+        self.alliance_name = result['alliance']['name']
+        self.members = result['members']['current']
+        self.ticker = result['ticker']
+        logger.info("Finished updating corp info for id %s from api." % self.id)
+        self.save()
 
 class EVEAlliance(models.Model):
-    id = models.CharField(max_length=254, primary_key=True)
-    name = models.CharField(max_length=254)
-    ticker = models.CharField(max_length=254)
+    id = models.PositiveIntegerField(primary_key=True)
+    name = models.CharField(max_length=254, null=True)
+    ticker = models.CharField(max_length=254, null=True)
     def __unicode__(self):
-        if self.alliance_name:
+        if self.name:
             return self.name.encode('utf-8')
         else:
             logger.warn("Alliance name missing in alliance models for id %s - returning id as __unicode__" % self.id)
             return self.id.encode('utf-8')
 
 class EVEApiKeyPair(models.Model):
-    id = models.CharField(max_length=254, primary_key=True)
+    id = models.PositiveIntegerField(primary_key=True)
     vcode = models.CharField(max_length=254)
     owner = models.ForeignKey('authentication.User', null=True)
     is_valid = models.NullBooleanField()
