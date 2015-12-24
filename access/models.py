bc5da3bc112d92218472e07827d4b123214b1444
Adarnof <adarnof@gmail.com>
2015-12-24 13:29:28 +0000

    Corrected ForeignKey to OneToOneField in access models. Experimenttion with generic relation access model.

--- access/models.py
+++ access/models.py
@@ -2,27 +2,38 @@ from __future__ import unicode_literals
 
 from django.db import models
 from eveonline.models import EVECharacter, EVECorporation, EVEAlliance, EVEStanding
+from django.contrib.contenttypes.fields import GenericForeignKey
+from django.contrib.contenttypes.models import ContentType
 
 class CorpAccess(models.Model):
-    corp = models.ForeignKey(EVECorporation, on_delete=models.CASCADE, unique=True)
+    corp = models.OneToOneField(EVECorporation, models.CASCADE)
     def __unicode__(self):
         output = 'Access for corp %s' % self.corp
         return output.encode('utf-8')
 
 class AllianceAccess(models.Model):
-    alliance = models.ForeignKey(EVEAlliance, on_delete=models.CASCADE, unique=True)
+    alliance = models.OneToOneField(EVEAlliance, models.CASCADE)
     def __unicode__(self):
         output = 'Access for alliance %s' % self.alliance
         return output.encode('utf-8')
 
 class CharacterAccess(models.Model):
-    character = models.ForeignKey(EVECharacter, on_delete=models.CASCADE, unique=True)
+    character = models.OneToOneField(EVECharacter, models.CASCADE)
     def __unicode__(self):
         output = 'Access for character %s' % self.character
         return output.encode('utf-8')
 
 class StandingAccess(models.Model):
-    standing = models.ForeignKey(EVEStanding, on_delete=models.CASCADE, unique=True)
+    standing = models.OneToOneField(EVEStanding, models.CASCADE)
     def __unicode__(self):
         output = 'Access for %s' % self.standing
+        return output.encode('utf-8')
+
+class Access(models.Model):
+    access_mode = models.NullBooleanField()
+    object_id = models.PositiveIntegerField()
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+    content_object = GenericForeignKey('content_type', 'object_id')
+    def __unicode__(self):
+        output = 'Access for %s' % self.content_object
         return output.encode('utf-8')
