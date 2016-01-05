26039d8a5914575ec629ce94a56d7169de3ab25f
Adarnof <adarnof@gmail.com>
2016-01-05 01:09:33 +0000

    Testing permission assignment.

--- access/tests.py
+++ access/tests.py
@@ -3,7 +3,9 @@ from .models import UserAccess, CharacterAccessRule, CorpAccessRule, AllianceAcc
 from authentication.models import User
 from eveonline.managers import EVEManager
 import os
-from .tasks import generate_useraccess_by_characteraccess, generate_useraccess_by_corpaccess, generate_useraccess_by_allianceaccess
+from .tasks import generate_useraccess_by_characteraccess, generate_useraccess_by_corpaccess, generate_useraccess_by_allianceaccess, assess_access, assign_access
+from django.contrib.auth.models import Permission
+from django.contrib.contenttypes.models import ContentType
 
 class UserAccessAssignmentTestCase(TestCase):
     good_char_id = 94677678
@@ -16,6 +18,7 @@ class UserAccessAssignmentTestCase(TestCase):
         self.char = EVEManager.get_character_by_id(self.good_char_id)
         self.corp = EVEManager.get_corp_by_id(self.corp_id)
         self.alliance = EVEManager.get_alliance_by_id(self.alliance_id)
+        Permission.objects.get_or_create(content_type=ContentType.objects.get_for_model(UserAccess), codename='site_access')
     
     def test_create_characteraccess(self):
         #ensure useraccess generated for correct user upon creation of characteraccess rule
@@ -55,3 +58,18 @@ class UserAccessAssignmentTestCase(TestCase):
         aa.delete()
         self.assertEqual(len(self.good_user.useraccess_set.all()), 0)
         self.assertEqual(len(self.bad_user.useraccess_set.all()), 0)
+
+    def test_assign_all_useraccess(self):
+        #generate access rules for all 3 levels
+        CharacterAccessRule.objects.create(character=self.char).save()
+        CorpAccessRule.objects.create(corp=self.corp).save()
+        AllianceAccessRule.objects.create(alliance=self.alliance).save()
+        #run full assessment for good user
+        assign_access(self.good_user)
+        desired_perm = Permission.objects.get(content_type=ContentType.objects.get_for_model(UserAccess), codename='site_access')
+        self.assertEqual(len(self.good_user.useraccess_set.all()), 3)
+        self.assertTrue(desired_perm in self.good_user.user_permissions.all())
+        #run full assessment for bad user
+        assign_access(self.bad_user)
+        self.assertEqual(len(self.bad_user.useraccess_set.all()), 0)        
+        self.assertFalse(desired_perm in self.bad_user.user_permissions.all())
